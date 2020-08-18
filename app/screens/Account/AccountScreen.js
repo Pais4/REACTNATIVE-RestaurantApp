e66a4369f64e4e00afcb7b34f870f1e403eb6b27
Mateo Serna <mateoslopera@gmail.com>
2020-08-18 12:21:24 -0500

    Loading Component

--- app/screens/Account/AccountScreen.js
+++ app/screens/Account/AccountScreen.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from "react";
 import { View, Text } from "react-native";
 import * as firebase from "firebase";
 
+/* Componentes */
+import { LoadingComponent } from "../../components/Loading";
+
 /* Screens */
 import { UserGuestScreen } from "./UserGuestScreen";
 import { UserLoggedScreen } from "./UserLoggedScreen";
@@ -9,14 +12,15 @@ import { UserLoggedScreen } from "./UserLoggedScreen";
 export const AccountScreen = () => {
   const [login, setLogin] = useState(null);
 
-  useEffect(() => {
-    firebase.auth().onAuthStateChanged((user) => {
-      console.log(user);
-      !user ? setLogin(false) : setLogin(true);
-    });
-  }, []);
+  // useEffect(() => {
+  //   firebase.auth().onAuthStateChanged((user) => {
+  //     console.log(user);
+  //     !user ? setLogin(false) : setLogin(true);
+  //   });
+  // }, []);
 
-  if (login === null) return <Text>Cargando...</Text>;
+  if (login === null)
+    return <LoadingComponent isVisible={true} text="Cargando.." />;
 
   return login ? <UserLoggedScreen /> : <UserGuestScreen />;
 };
